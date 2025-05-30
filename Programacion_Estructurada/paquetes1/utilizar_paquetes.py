9cc9617945b2fcea21192e16864a276906d0c2ee
AguilarMarcos <marcosuriel331@gmail.com>
2025-05-30 17:26:49 -0600

    Add files via upload

---
+++ Programacion_Estructurada/paquetes1/utilizar_paquetes.py
@@ -0,0 +1,7 @@
+from paquete1 import modulos
+
+print(modulos.saludar("Daniel Contreras Ruano"))
+
+modulos.borrarpantalla()
+nom,tel=modulos.solicitarDatos2()
+print(f"\n\t,:: agenda telefonica ::,\n\t\tnobre:")
